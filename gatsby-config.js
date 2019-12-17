805a1ab4a6ed8c55bf9f1a73aabe885bd96563d8
Sawyer-Middeleer <sawyerm@uchicago.edu>
2019-12-17 15:24:57 -0600

    added google analytics to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: "UA-154680091-1",
       },
     },
     `gatsby-plugin-feed`,
