cfba854f2dbaaa539a6684a1492694ef0e5f62a9
Sawyer-Middeleer <saw.midd@gmail.com>
2022-01-03 22:11:05 -0800

    scss updates

--- src/components/tag.js
+++ src/components/tag.js
@@ -1,18 +1,21 @@
 import React from "react"
 
+// const Tag = ({ postType }) => {
+//   if (postType === "Civic Tech") {
+//     return <small class="btn-civic-tech">{postType}</small>
+//   }
+//   else if (postType === "Web3") {
+//     return <small class="btn-web3">{postType}</small>
+//   }
+//   else if (postType === "Product Management") {
+//     return <small class="btn-product">{postType}</small>
+//   }
+//   else if (postType === "Public Policy") {
+//     return <small class="btn-policy">{postType}</small>
+//   }
+// };
 const Tag = ({ postType }) => {
-  if (postType === "Civic Tech") {
-    return <small class="btn-civic-tech">{postType}</small>
-  }
-  else if (postType === "Web3") {
-    return <small class="btn-web3">{postType}</small>
-  }
-  else if (postType === "Product Management") {
-    return <small class="btn-product">{postType}</small>
-  }
-  else if (postType === "Public Policy") {
-    return <small class="btn-policy">{postType}</small>
-  }
+  return <small class="btn-tag">{postType}</small>
 };
 
 export default Tag
